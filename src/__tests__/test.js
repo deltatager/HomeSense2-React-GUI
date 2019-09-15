c9b293b9b5e70f68833b11cfa08c3d0bd847b2cf
deltatager <simonllandry@hotmail.com>
2019-09-15 11:09:33 -0400

    Trying selenium qith CI env set to true

--- src/__tests__/test.js
+++ src/__tests__/test.js
@@ -1,28 +1,23 @@
-// const {Builder, By} = require('selenium-webdriver');
-//
-// let driver;
-//
-// beforeAll(() => {
-//
-//     driver = new Builder()
-//         .forBrowser('firefox')
-//         .usingServer('http://192.168.11.18:4444/wd/hub')
-//         .build();
-// });
-//
-// afterAll(() => {
-//     driver.quit();
-// });
-//
-// it('Changing google.com language', async () => {
-//     await driver.get('https://www.google.com');
-//     await driver.findElement(By.id('SIvCob')).findElement(By.css('a')).click();
-//     let actual = await driver.findElement(By.id('SIvCob')).getText();
-//     let expected = 'Google disponible en : English';
-//     expect(actual).toEqual(expected);
-// }, 100000);
+const {Builder, By} = require('selenium-webdriver');
 
-it('Test', () => {
-    expect(5).toEqual(5);
-    }
-);
+let driver;
+
+beforeAll(() => {
+
+    driver = new Builder()
+        .forBrowser('firefox')
+        .usingServer('http://192.168.11.18:4444/wd/hub')
+        .build();
+});
+
+afterAll(() => {
+    driver.quit();
+});
+
+it('Changing google.com language', async () => {
+    await driver.get('https://www.google.com');
+    await driver.findElement(By.id('SIvCob')).findElement(By.css('a')).click();
+    let actual = await driver.findElement(By.id('SIvCob')).getText();
+    let expected = 'Google disponible en : English';
+    expect(actual).toEqual(expected);
+}, 100000);
